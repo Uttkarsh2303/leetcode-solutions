f2af546f19f256c973536bd99051baaf3d54f081
Uttkarsh2303 <uttkarshgupta9677@gmail.com>
2026-07-20 13:58:10 +0530

    Time: 0 ms (100%), Space: 12.3 MB (54.44%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        max_candies=max(candies)
+        ans=[]
+        for i in candies:
+            if i+extraCandies>=max_candies:
+                ans.append(True)
+            else:
+                ans.append(False)
+
+        return ans
+            
+        
