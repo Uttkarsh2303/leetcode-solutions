e89f1ba0a9ff7f1a18e7266c8b7e850413b4c947
Uttkarsh2303 <uttkarshgupta9677@gmail.com>
2026-09-11 00:21:57 +0530

    Time: 27 ms (23.97%), Space: 19.7 MB (35.3%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,36 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
+        results=[]
+        def is_safe(row,col):
+            for r in range(row):
+                if board[r][col]=='Q':
+                    return False
+            r=row-1
+            c=col-1
+            while r>=0 and c>=0:
+                if board[r][c]=='Q':
+                    return False
+                r-=1
+                c-=1
+            r = row - 1
+            c = col + 1
+
+            while r >= 0 and c < n:
+                if board[r][c] == "Q":
+                    return False
+                r -= 1
+                c += 1
+            return True
+        def backtrack(row):
+            if row==n:
+                results.append([''.join(row) for row in board])
+                return board
+            for col in range(n):
+                if not is_safe(row,col):
+                    continue
+                board[row][col]='Q'
+                backtrack(row+1)
+                board[row][col]='.'
+        backtrack(0)
+        return results
