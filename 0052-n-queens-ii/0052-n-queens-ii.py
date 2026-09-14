2e6ff2787c97c664d3f30aaf1277f265d18f68ba
Uttkarsh2303 <uttkarshgupta9677@gmail.com>
2026-09-14 13:23:02 +0530

    Time: 27 ms (12.74%), Space: 19.5 MB (21.59%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,39 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        board = [["."] * n for _ in range(n)]
+        count=0
+        def is_safe(row,col):
+            for r in range(row):
+                if board[r][col]=='Q':
+                    return False
+            r=row-1
+            c=col-1
+            while r>=0 and c>=0:
+                if board[r][c]=='Q':
+                    return False
+                r-=1
+                c-=1
+            r = row - 1
+            c = col + 1
+
+            while r >= 0 and c < n:
+                if board[r][c] == "Q":
+                    return False
+                r -= 1
+                c += 1
+            return True
+        def backtrack(row):
+            nonlocal count
+            if row==n:
+                count+=1
+                return 
+            for col in range(n):
+                if not is_safe(row,col):
+                    continue
+                board[row][col]='Q'
+                backtrack(row+1)
+                board[row][col]='.'
+            
+        backtrack(0)
+        
+        return count
